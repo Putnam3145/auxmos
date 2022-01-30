87388746a6cefa589362ce2069eaa52c9b6aabe6
Katherine Kiefer <sterlite2902@gmail.com>
2022-01-30 19:48:49 +1100

    refs here

--- src/lib.rs
+++ src/lib.rs
@@ -255,7 +255,7 @@ fn _set_moles_hook() {
 fn _adjust_moles_hook(id_val: Value, num_val: Value) {
 	let vf = num_val.as_number().unwrap_or_default();
 	with_mix_mut(src, |mix| {
-		mix.adjust_moles(gas_idx_from_value(id_val)?, vf);
+		mix.adjust_moles(gas_idx_from_value(&id_val)?, vf);
 		Ok(Value::null())
 	})
 }
@@ -284,7 +284,7 @@ fn _scrub_into_hook(into: Value, ratio_v: Value, gas_list: Value) {
 	let gas_scrub_vec = (1..=gases_to_scrub.len())
 		.filter_map(|idx| gas_idx_from_value(&gases_to_scrub.get(idx).unwrap()).ok())
 		.collect::<Vec<_>>();
-	with_mixes_mut(src, into, |src_gas, dest_gas| {
+	with_mixes_mut(src, &into, |src_gas, dest_gas| {
 		src_gas.transfer_gases_to(ratio, &gas_scrub_vec, dest_gas);
 		Ok(Value::from(true))
 	})
@@ -337,7 +337,7 @@ fn _react_hook(holder: Value) {
 	let mut ret: i32 = 0;
 	let reactions = with_mix(src, |mix| Ok(mix.all_reactable()))?;
 	for reaction in reactions {
-		ret |= react_by_id(reaction, src, holder)?
+		ret |= react_by_id(reaction, src, &holder)?
 			.as_number()
 			.unwrap_or_default() as i32;
 		if ret & STOP_REACTIONS == STOP_REACTIONS {
@@ -369,7 +369,7 @@ fn _adjust_heat_hook() {
 
 #[hook("/datum/gas_mixture/proc/transfer_to")]
 fn _transfer_hook(other: Value, moles: Value) {
-	with_mixes_mut(src, other, |our_mix, other_mix| {
+	with_mixes_mut(src, &other, |our_mix, other_mix| {
 		other_mix.merge(&our_mix.remove(moles.as_number().map_err(|_| {
 			runtime!(
 				"Attempt to interpret non-number value as number {} {}:{}",
@@ -384,7 +384,7 @@ fn _transfer_hook(other: Value, moles: Value) {
 
 #[hook("/datum/gas_mixture/proc/transfer_ratio_to")]
 fn _transfer_ratio_hook(other: Value, ratio: Value) {
-	with_mixes_mut(src, other, |our_mix, other_mix| {
+	with_mixes_mut(src, &other, |our_mix, other_mix| {
 		other_mix.merge(&our_mix.remove_ratio(ratio.as_number().map_err(|_| {
 			runtime!(
 				"Attempt to interpret non-number value as number {} {}:{}",

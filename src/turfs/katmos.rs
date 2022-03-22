8acd6a70f5aac3b4bea4c10b8b92b9955948b22c
Katherine Kiefer <sterlite2902@gmail.com>
2022-03-22 12:05:12 +1100

    refstuff

--- src/turfs/katmos.rs
+++ src/turfs/katmos.rs
@@ -643,6 +643,7 @@ fn flood_fill_equalize_turfs(
 					let result = turf_gases().try_get(&loc);
 					if result.is_locked() {
 						ignore_zone = true;
+						continue
 					}
 					if let Some(adj_turf) = result.try_unwrap() {
 						if adj_turf.enabled() {
